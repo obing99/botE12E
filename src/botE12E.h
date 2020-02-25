3e420478b33de0f9ea916517da5a990da69efa2b
TEKNIK ELEKTRONIKA SMK GAJAH MADA BANYUWANGI <32142607+Elka68@users.noreply.github.com>
2020-02-26 00:12:24 +0700

    Update botE12E.h

--- src/botE12E.h
+++ src/botE12E.h
@@ -1,22 +1,29 @@
+#include <Arduino.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
-HTTPClient http;
+#include <WiFiClientSecureBearSSL.h>
+const uint8_t fingerprint[20] = {0xbb, 0xdc, 0x45, 0x2a, 0x07, 0xe3,0x4a, 0x71, 0x33, 0x40, 0x32, 0xda, 0xbe, 0x81, 0xf7, 0x72, 0x6f, 0x4a, 0x2b, 0x6b};
+ESP8266WiFiMulti WiFiMulti;
+HTTPClient https;
 
-void koneksiWifi(String NAMA ,String PSWD){
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(NAMA, PSWD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+void koneksiWifi(const char* NAMA ,const char* PSWD){
+ WiFi.mode(WIFI_STA);
+ WiFiMulti.addAP(NAMA , PSWD);
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
   }
 }
 
 String bacaPesan(String botTOKEN) {
- String myurl= "https://api.telegram.org/" + botTOKEN+" /getUpdates?offset=-1";
- http.begin(myurl,"BB:DC:45:2A:07:E3:4A:71:33:40:32:DA:BE:81:F7:72:6F:4A:2B:6B");
- http.GET();
- String hasil = http.getString();
- http.end();
+ std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+ client->setFingerprint(fingerprint);
+ String myurl = "https://api.telegram.org/" + botTOKEN + "/getUpdates?offset=-1";
+ https.begin(*client , myurl);
+ https.GET();
+ String hasil = https.getString();
+ https.end();
  DynamicJsonDocument doc(1024);
  deserializeJson(doc, hasil);
  String isiPesan = doc["result"][0]["message"]["text"]; 
@@ -24,8 +31,10 @@ String bacaPesan(String botTOKEN) {
 }
 
 void kirimPesan(String Pesan, String botTOKEN, String ID){
+ std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+ client->setFingerprint(fingerprint);
  String url = "https://api.telegram.org/" + botTOKEN + "/sendMessage?chat_id=" + ID + "&text=" + Pesan;
- http.begin(url,"BB:DC:45:2A:07:E3:4A:71:33:40:32:DA:BE:81:F7:72:6F:4A:2B:6B");
- http.GET();
- http.end();
+ https.begin(*client , url);
+ https.GET();
+ https.end();
 }
